d053466139c075db17f070b7d2cd874e4b659998
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-08-21 18:21:51 +0200

    Rebase from 'release/lyrical/jrl_cmakemodules'

--- flake.nix
+++ flake.nix
@@ -9,11 +9,23 @@
       { lib, ... }:
       {
         overrideAttrs = {
-          jrl-cmakemodules = {
+          jrl-cmakemodules = { pkgs-final, drv-prev, ... }: {
             src = lib.fileset.toSource {
               root = ./.;
               fileset = lib.fileset.gitTracked ./.;
             };
+            cmakeFlags = drv-prev.cmakeFlags ++ [
+              (lib.cmakeBool "JRL_CMAKEMODULES_ENABLE_TEST_CPPAD" true)
+              (lib.cmakeBool "JRL_CMAKEMODULES_ENABLE_TEST_CPPADCG" true)
+              (lib.cmakeBool "JRL_CMAKEMODULES_ENABLE_TEST_GMP" true)
+              (lib.cmakeBool "JRL_CMAKEMODULES_ENABLE_TEST_MPFR" true)
+            ];
+            checkInputs = drv-prev.checkInputs ++ [
+              pkgs-final.cppad
+              pkgs-final.cppadcodegen
+              pkgs-final.gmp
+              pkgs-final.mpfr
+            ];
           };
           jrl-cmakemodules-scripts = { pkgs-final, drv-prev, ... }: {
             nativeBuildInputs = drv-prev.nativeBuildInputs ++ [
